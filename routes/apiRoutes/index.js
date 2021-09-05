0b8c0c77432e66fe30e4f43eae30d577eb0fae2c
Ryan Dungan <rd3@knights.ucf.edu>
2021-09-04 20:11:23 -0400

    added voter routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const apiRoutes = require('./routes/apiRoutes');
+
+router.use(require('./voterRoutes'));
+
+router.use(require('./candidateRoutes'));
+
+router.use(require('./partyRoutes'));
+
+app.use('/api', apiRoutes);
+
+module.exports = router;
