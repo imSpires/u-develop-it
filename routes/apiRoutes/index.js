c129034e43525122306b7ca6f34f1d774287d05b
Ryan Dungan <rd3@knights.ucf.edu>
2021-09-05 12:00:00 -0400

    final touches to API routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const apiRoutes = require('./routes/apiRoutes');
-
-router.use(require('./voterRoutes'));
 
 router.use(require('./candidateRoutes'));
-
 router.use(require('./partyRoutes'));
-
+router.use(require('./voterRoutes'));
 router.use(require('./voteRoutes'));
-
-app.use('/api', apiRoutes);
 
 module.exports = router;
